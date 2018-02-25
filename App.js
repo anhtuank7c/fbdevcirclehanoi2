10b5a03397dd8dba1c3e6027481a43c132b53c3c
Anh Tuan Nguyen <anhtuank7c@hotmail.com>
2018-02-26 00:29:50 +0700

    Moving custom module into components

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  NativeModules
-} from 'react-native';
-
-const MyCustomModule = NativeModules.MyCustomModule;
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import MyCustomModule from './components/MyCustomModule';
 
 type Props = {};
 export default class App extends Component<Props> {
